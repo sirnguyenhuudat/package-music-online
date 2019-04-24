bb1aa1022dd90b260c3cb7b7125402c66096ac95
Nguyen Huu Dat <sr.nguyenhuudat@gmail.com>
2019-04-24 08:26:56 +0700

    add track datatable

--- js/backend_script.js
+++ js/backend_script.js
@@ -1,5 +1,6 @@
 $(document).ready( function () {
     $('#genreTable').DataTable();
     $('#artistTable').DataTable();
+    $('#trackTable').DataTable();
     $('.artist_search_genre_multi').select2();
 });
