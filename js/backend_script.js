e9e08829e5a45fbddae4e447ba8c66cfdfa482ff
Nguyen Huu Dat <sr.nguyenhuudat@gmail.com>
2019-04-25 10:33:19 +0700

    add track datatable and select2

--- js/backend_script.js
+++ js/backend_script.js
@@ -2,6 +2,8 @@ $(document).ready( function () {
     $('#genreTable').DataTable();
     $('#artistTable').DataTable();
     $('#trackTable').DataTable();
+    $('#albumTable').DataTable();
     $('.artist_search_genre_multi').select2();
     $('.track_search_genre_multi').select2();
+    $('.album_search_track_multi').select2();
 });
